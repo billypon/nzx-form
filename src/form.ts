59230c133286d71c17a80cead85190ed2067e835
billypon <billypon@gmail.com>
2019-06-19 18:14:35 +0800

    form class support group change callback

--- src/form.ts
+++ src/form.ts
@@ -5,10 +5,21 @@ import { FormState, EmptyFormGroup } from './form.types';
 
 export class NzxForm {
   state: Dictionary<FormState>;
-  group: FormGroup = EmptyFormGroup;
   submit: () => void;
   value: Dictionary = { };
+  groupChange: () => void;
   layout: string = 'horizontal';
   class: string | Dictionary<boolean> = '';
   loading: boolean;
+
+  private _group: FormGroup = EmptyFormGroup;
+  get group(): FormGroup {
+    return this._group;
+  }
+  set group(value: FormGroup) {
+    this._group = value;
+    if (this.groupChange) {
+      this.groupChange();
+    }
+  }
 }
